85bc824b63a34652101ed7c0b5270d9bdb6407c3
Vinicius Bloise <vinicius.bloise@gmail.com>
2019-01-15 04:00:55 +0100

    missing files

---
+++ UCTS.CLI/ICarOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UCTS.CLI
+{
+    public interface ICarOperations
+    {
+        Task<string> ReportAsync(string car_name);
+        void NewCarAsync(string car_type, string car_name);
+        void RemoveCarAsync(string car_name);
+        void SetAsync(string car_name, string attr, string value);
+    }
+
+    public interface IGetOperations
+    {
+        ICarOperations Operations { get; }
+    }
+}
---
+++ UCTS.ManagerService/Hubs/RadioHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace UCTS.ManagerService.Hubs
+{
+    public class RadioHub : Hub, IRadioHub
+    {
+        public async Task AddCar(string sender, string car_name)
+        {
+            await Clients.All.SendAsync("carAdded", sender, car_name);
+        }
+        public async Task RemoveCar(string sender, string car_name)
+        {
+            await Clients.All.SendAsync("carRemoved", sender, car_name);
+        }
+        public async Task NewMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+
+    public interface IRadioHub
+    {
+        Task AddCar(string sender, string car_name);
+        Task RemoveCar(string sender, string car_name);
+        Task NewMessage(string username, string message);
+    }
+}
